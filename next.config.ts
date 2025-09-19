3e992af73cc812ba00c6c5c5b6913025f9e8e00f
Emad Lashkari <emadlashkari.82@gmail.com>
2025-09-19 20:03:41 +0330

    change package json settings

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/jewellery-shop",
+  basePath: isProd ? "/jewellery-shop" : "",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/jewellery-shop/" : "",
 };
 
 export default nextConfig;
