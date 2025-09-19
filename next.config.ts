3307407f18585464765f3fbb81f36982d64458f0
Emad Lashkari <emadlashkari.82@gmail.com>
2025-09-19 19:46:35 +0330

    try to fix path issue

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   basePath: "/jewellery-shop",
-  assetPrefix: "/jewellery-shop/",
 };
 
 export default nextConfig;
